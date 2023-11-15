c31c4b93b320dbad3d80ae4157748dcf7f6aab7b
Skynse <prodbyskynse@gmail.com>
2023-11-15 16:53:57 -0600

    project screen coords to world coords

--- silica_ui/src/main.rs
+++ silica_ui/src/main.rs
@@ -4,16 +4,22 @@ use macroquad::ui::{hash, root_ui, widgets};
 use silica_engine::prelude::*;
 use std::convert::TryInto;
 
-#[macroquad::main("BasicShapes")]
+fn window_conf() -> Conf {
+    Conf {
+        window_title: "Silica".to_owned(),
+        window_width: 1280,
+        window_height: 920,
+        ..Default::default()
+    }
+}
+#[macroquad::main(window_conf)]
 async fn main() {
+    let scale_fac = 1.5;
     let w: usize = 611;
     let h: usize = 383;
     let mut image = Image::gen_image_color(w as u16, h as u16, BLACK);
     let mut world: World = World::new(w.clone(), h.clone());
     let texture = Texture2D::from_image(&image);
-
-    let scale_x = screen_width() / w as f32;
-    let scale_y = screen_height() / h as f32;
 
     draw_walls(&mut world);
 
@@ -34,15 +40,18 @@ async fn main() {
             }
         }
 
-        let (mouse_x, mouse_y) = mouse_position();
+        let mouse_pos = mouse_position();
+        let mouse_x = mouse_pos.0 as usize;
+        let mouse_y = mouse_pos.1 as usize;
+
+        // convert screen coords to world coords for mouse
+        let (screen_w, screen_h) = screen_size();
+        let mouse_x_world = (mouse_x as f32 / screen_w * w as f32) as usize;
+        let mouse_y_world = (mouse_y as f32 / screen_h * h as f32) as usize;
 
         if is_mouse_button_down(MouseButton::Left) {
             // use screen coords mapped to world coords
-            world.set_particle(
-                mouse_x as i32 / screen_width() as i32 * w as i32,
-                mouse_y as i32 / screen_height() as i32 * h as i32,
-                Variant::Sand,
-            );
+            world.set_particle(mouse_x_world as i32, mouse_y_world as i32, Variant::Sand);
         }
         for touch in touches() {
             println!("Touch at {}, {}", touch.position.x, touch.position.y);
@@ -59,7 +68,7 @@ async fn main() {
             0.,
             WHITE,
             DrawTextureParams {
-                dest_size: Some(vec2(screen_width() * scale_x, screen_height() * scale_y)),
+                dest_size: Some(vec2(screen_width(), screen_height())),
                 source: Some(Rect::new(0.0, 0.0, w as f32, h as f32)),
                 ..Default::default()
             },
