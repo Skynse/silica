5f3cf5be13dccf90f00a93a2be39fb40ff928700
Skynse <prodbyskynse@gmail.com>
2023-11-15 18:46:59 -0600

    implement basic UI

--- silica_ui/src/main.rs
+++ silica_ui/src/main.rs
@@ -1,8 +1,10 @@
 use macroquad::miniquad::window::screen_size;
 use macroquad::prelude::*;
-use macroquad::ui::{hash, root_ui, widgets};
-use silica_engine::prelude::*;
+use macroquad::ui::{hash, root_ui, widgets, Skin};
+use silica_engine::group::ElementManager;
+use silica_engine::{prelude::*, world};
 use std::convert::TryInto;
+use std::ops::AddAssign;
 
 fn window_conf() -> Conf {
     Conf {
@@ -12,18 +14,47 @@ fn window_conf() -> Conf {
         ..Default::default()
     }
 }
+
+struct GameProperties {
+    tool_radius: f32,
+    tool_type: Variant,
+    selected_group_idx: usize,
+}
+
+struct WorldInfo {
+    fps: i32,
+    properties: GameProperties,
+    world_width: usize,
+    world_height: usize,
+}
+
 #[macroquad::main(window_conf)]
 async fn main() {
-    let scale_fac = 1.5;
+    // Initialization
+
     let w: usize = 611;
     let h: usize = 383;
     let mut image = Image::gen_image_color(w as u16, h as u16, BLACK);
     let mut world: World = World::new(w.clone(), h.clone());
     let texture = Texture2D::from_image(&image);
-
+    let game_properties = GameProperties {
+        tool_radius: 10.0,
+        tool_type: Variant::Sand,
+        selected_group_idx: 0,
+    };
+    let mut world_info = WorldInfo {
+        fps: 0,
+        properties: game_properties,
+        world_width: w,
+        world_height: h,
+    };
     draw_walls(&mut world);
 
+    let element_manager: ElementManager = ElementManager::new();
+    register_element_groups(&element_manager);
+
     loop {
+        world_info.fps = get_fps();
         clear_background(BLACK);
         world.tick();
         let w = image.width();
@@ -34,8 +65,7 @@ async fn main() {
                 let particle = world.get_particle(x as i32, y as i32);
                 let color = particle_to_color(particle.variant);
                 let c = color_u8!(color.0, color.1, color.2, 255);
-                image.set_pixel(x as u32, y as u32, c);
-                image.set_pixel(x.try_into().unwrap(), y.try_into().unwrap(), c);
+
                 image.set_pixel(x, y, c);
             }
         }
@@ -49,9 +79,35 @@ async fn main() {
         let mouse_x_world = (mouse_x as f32 / screen_w * w as f32) as usize;
         let mouse_y_world = (mouse_y as f32 / screen_h * h as f32) as usize;
 
+        let mouse_wheel = mouse_wheel().1;
+        world_info.properties.tool_radius += mouse_wheel * 4.;
+
+        // handle input
         if is_mouse_button_down(MouseButton::Left) {
             // use screen coords mapped to world coords
-            world.set_particle(mouse_x_world as i32, mouse_y_world as i32, Variant::Sand);
+            // make sure that the particle at the mouse position is empty
+            if world
+                .get_particle(mouse_x_world as i32, mouse_y_world as i32)
+                .variant
+                == Variant::Empty
+            {
+                paint_radius(
+                    &mut world,
+                    mouse_x_world as i32,
+                    mouse_y_world as i32,
+                    world_info.properties.tool_type,
+                    world_info.properties.tool_radius as i32,
+                );
+            }
+        }
+
+        if is_mouse_button_down(MouseButton::Right) {
+            erase_radius(
+                &mut world,
+                mouse_x_world as i32,
+                mouse_y_world as i32,
+                world_info.properties.tool_radius as i32,
+            );
         }
         for touch in touches() {
             println!("Touch at {}, {}", touch.position.x, touch.position.y);
@@ -74,6 +130,11 @@ async fn main() {
             },
         );
 
+        draw_top_panel(&mut world_info);
+        draw_tool_outline(&mut world_info);
+        draw_group_sidebar(&element_manager, &mut world_info);
+        draw_element_list(&element_manager, &mut world_info);
+
         next_frame().await
     }
 }
@@ -87,4 +148,135 @@ fn draw_walls(world: &mut World) {
         world.set_particle(0, y as i32, Variant::Wall);
         world.set_particle(world.width as i32 - 1, y as i32, Variant::Wall);
     }
+}
+
+fn draw_group_sidebar(manager: &ElementManager, world_info: &mut WorldInfo) {
+    let mut ui = root_ui().window(
+        hash!(),
+        vec2(screen_width() - 200.0, 30.0),
+        vec2(200.0, screen_height() - 30.0),
+        |ui| {
+            ui.label(None, "Groups");
+            for (idx, group) in manager.groups.borrow().iter().enumerate() {
+                let button = widgets::Button::new(group.group_name.clone())
+                    .position(vec2(0.0, 30.0 * (idx + 1) as f32))
+                    .size(vec2(200.0, 30.0))
+                    .ui(ui);
+                if button {
+                    world_info.properties.selected_group_idx = idx;
+                }
+            }
+        },
+    );
+}
+
+// horizontal left to right assortment of elements
+// shows up on bottom of screen
+// truncated names to 4 chars
+fn draw_element_list(manager: &ElementManager, world_info: &mut WorldInfo) {
+    let mut ui = root_ui().window(
+        hash!(),
+        vec2(0.0, screen_height() - 30.0),
+        vec2(screen_width(), 30.0),
+        |ui| {
+            let binding = manager.groups.borrow();
+            let group = binding
+                .get(world_info.properties.selected_group_idx)
+                .unwrap();
+            let elements = group.get_elements();
+            let mut x = 0.0;
+            for element in elements {
+                let button = widgets::Button::new(element.to_string())
+                    .position(vec2(x, 0.0))
+                    .size(vec2(100.0, 30.0))
+                    .ui(ui);
+
+                // if button is selected, draw a rectangle around it
+                if world_info.properties.tool_type == element {
+                    draw_rectangle_lines(
+                        x,
+                        screen_height() - 30.0,
+                        100.0,
+                        30.0,
+                        2.0,
+                        Color::new(1.0, 1.0, 1.0, 1.0),
+                    );
+                }
+                if button {
+                    world_info.properties.tool_type = element;
+                }
+                x += 100.0;
+            }
+        },
+    );
+}
+fn draw_top_panel(world_info: &mut WorldInfo) {
+    let panel_height = 30.0; // Adjust the height of the top panel as needed
+
+    // Draw a colored rectangle at the top to represent the panel
+    draw_rectangle(
+        0.0,
+        0.0,
+        screen_width(),
+        panel_height,
+        Color::new(50.0 / 255.0, 50.0 / 255.0, 50.0 / 255.0, 0.0), // Adjust the color as needed
+    );
+
+    // Draw text or other UI elements on the top panel
+    draw_text(
+        &format!("FPS: {:.2}", world_info.fps), // Display FPS with two decimal places
+        20.0,
+        20.0,
+        30.0,
+        Color::new(1.0, 1.0, 1.0, 1.0), // Adjust the color as needed
+    );
+
+    // draw currently selected tool to the right
+    draw_text(
+        &format!("Tool: {:?}", world_info.properties.tool_type),
+        screen_width() - 200.0,
+        20.0,
+        30.0,
+        Color::new(1.0, 1.0, 1.0, 1.0), // Adjust the color as needed
+    );
+}
+
+fn draw_tool_outline(world_info: &mut WorldInfo) {
+    // get mouse pos
+    let (mouse_x, mouse_y) = mouse_position();
+    let mouse_x = mouse_x as usize;
+    let radius = world_info.properties.tool_radius;
+    draw_circle(
+        mouse_x as f32,
+        mouse_y as f32,
+        radius,
+        Color::new(1.0, 1.0, 1.0, 1.0),
+    );
+}
+
+fn paint_radius(world: &mut World, x: i32, y: i32, variant: Variant, radius: i32) {
+    for dx in -radius..radius {
+        for dy in -radius..radius {
+            if dx * dx + dy * dy < radius * radius {
+                world.set_particle(x + dx, y + dy, variant);
+            }
+        }
+    }
+}
+
+fn erase_radius(world: &mut World, x: i32, y: i32, radius: i32) {
+    for dx in -radius..radius {
+        for dy in -radius..radius {
+            if dx * dx + dy * dy < radius * radius {
+                world.set_particle(x + dx, y + dy, Variant::Empty);
+            }
+        }
+    }
+}
+
+fn register_element_groups(manager: &ElementManager) {
+    manager.register_group(
+        "Powders",
+        vec![Variant::Sand, Variant::Water, Variant::Fire],
+    )
 }
